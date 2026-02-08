cb36c06ceb013eed0d2716fd878287453ec6e1fb
valued mammal <valuedmammal@protonmail.com>
2026-02-08 00:51:30 -0500

    bench: Add `block_graph_checkpoint`

---
+++ block-graph/benches/block_graph_checkpoint.rs
@@ -0,0 +1,88 @@
+//! Benchmark `CheckPoint::get` with a query height near the middle of checkpoint of varying lengths.
+
+use criterion::Criterion;
+use criterion::{black_box, criterion_group, criterion_main};
+
+use block_graph::checkpoint::CheckPoint;
+
+// Call `get` with the given query.
+fn bench_cp(cp: &CheckPoint<i32>, query_height: u32) {
+    assert!(cp.get(query_height).is_some(), "`query_height` should exist in `cp`");
+}
+
+fn checkpoint_2_pow_17(c: &mut Criterion) {
+    let mut cp = CheckPoint::new(0, 0);
+
+    let len: u32 = 1 << 17;
+    let query_height = len / 2;
+
+    for i in 1..len {
+        let height = i;
+        let value = height as i32;
+        cp = cp.push(height, value).unwrap();
+    }
+
+    c.bench_function("checkpoint_2_pow_17", move |b| {
+        b.iter(|| bench_cp(black_box(&cp), black_box(query_height)));
+    });
+}
+
+fn checkpoint_2_pow_18(c: &mut Criterion) {
+    let mut cp = CheckPoint::new(0, 0);
+
+    let len: u32 = 1 << 18;
+    let query_height = len / 2;
+
+    for i in 1..len {
+        let height = i;
+        let value = height as i32;
+        cp = cp.push(height, value).unwrap();
+    }
+
+    c.bench_function("checkpoint_2_pow_18", move |b| {
+        b.iter(|| bench_cp(black_box(&cp), black_box(query_height)));
+    });
+}
+
+fn checkpoint_2_pow_19(c: &mut Criterion) {
+    let mut cp = CheckPoint::new(0, 0);
+
+    let len: u32 = 1 << 19;
+    let query_height = len / 2;
+
+    for i in 1..len {
+        let height = i;
+        let value = height as i32;
+        cp = cp.push(height, value).unwrap();
+    }
+
+    c.bench_function("checkpoint_2_pow_19", move |b| {
+        b.iter(|| bench_cp(black_box(&cp), black_box(query_height)));
+    });
+}
+
+fn checkpoint_2_pow_20(c: &mut Criterion) {
+    let mut cp = CheckPoint::new(0, 0);
+
+    let len: u32 = 1 << 20;
+    let query_height = len / 2;
+
+    for i in 1..len {
+        let height = i;
+        let value = height as i32;
+        cp = cp.push(height, value).unwrap();
+    }
+
+    c.bench_function("checkpoint_2_pow_20", move |b| {
+        b.iter(|| bench_cp(black_box(&cp), black_box(query_height)));
+    });
+}
+
+criterion_group!(
+    benches,
+    checkpoint_2_pow_17,
+    checkpoint_2_pow_18,
+    checkpoint_2_pow_19,
+    checkpoint_2_pow_20
+);
+criterion_main!(benches);
